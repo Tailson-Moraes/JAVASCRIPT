e852189adf673198ae84f7dd2d41161e8eae9e47
Tailson-Moraes <tailsondevops@gmail.com>
2025-08-09 12:00:02 -0300

    finalizando css / iniciando JS

--- PROJETOS/To-do List/script.js
+++ PROJETOS/To-do List/script.js
@@ -1,7 +1,19 @@
-let loyalt = document.querySelector(".loyalt")
-
-let loyaltClone = loyalt.cloneNode(true);
 
 
+//Clonando loyalt
 
-console.log(loyaltClone)
+function addTask(){
+    let tarefaName = document.querySelector("#tarefa");
+
+    console.log(tarefaName.value)
+}
+
+
+
+
+let btn = document.querySelector("#button-adicionar");
+
+btn.addEventListener("click",(e)=>{
+    e.preventDefault();
+    addTask();
+})
